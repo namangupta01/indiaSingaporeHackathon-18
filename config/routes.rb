8a7fbe17c5cb65abeecbd792219ea53cd538d6e2
Naman Gupta <01namangupta@gmail.com>
2018-11-13 02:38:58 +0530

    Minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
 post 'notification/set-registration-token' => 'notification#set_registraion_token'
 post 'notification/send-help' => 'notification#send_help'
-post 'notification/google-home' => 'notification#google_home'
+post 'notification/google-action' => 'notification#google_home'
 post 'notification/upload-image-notification' => 'notification#upload_unknown_image_send_notification'
 get 'notification/get-notifications' => 'notification#get_notifications'
 post 'notification/fighting-notification' => 'notification#fighting_notification'
